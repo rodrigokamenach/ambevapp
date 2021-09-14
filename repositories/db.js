8b50f30772ad2b049daff846b13596fcecc321e9
Rodrigo Kamenach <rodrigokamenach@hotmail.com>
2021-09-14 18:01:22 -0300

    alterado conexao do banco

--- repositories/db.js
+++ repositories/db.js
@@ -1,23 +1,23 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize(	
-	'ambevapp', 'postgres', 'Wsi@@2021', {
-        dialect: "postgres",
-        define: {
-            timestamps: false
-        }
-    }
-);
-
-// const sequelize = new Sequelize(
-//     "postgres://postgres:Wsi@@2021@34.204.162.68/ambevapp",
-//     {
+// const sequelize = new Sequelize(	
+// 	'ambevapp', 'postgres', 'Wsi@@2021', {
 //         dialect: "postgres",
 //         define: {
 //             timestamps: false
 //         }
 //     }
 // );
+
+const sequelize = new Sequelize(
+    "postgres://postgres:Wsi@@2021@34.204.162.68/ambevapp",
+    {
+        dialect: "postgres",
+        define: {
+            timestamps: false
+        }
+    }
+);
 
 
 export default sequelize;
